40560672a2fca26951e05fa0e8e8c532db2f281a
Franziska Chen <chenfranziska@gmail.com>
2024-05-22 15:44:32 +0200

    Clone manually the structure from landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,147 @@
-import './App.css';
-import logo from './logo.svg';
+import styles from './App.module.scss';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <body>
+      <a href="/">
+        <img src="/" alt="logo" />
+        <h1>
+          Magician <span>beta</span>
+        </h1>
+      </a>
+
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Features</a>
+            </li>
+            <li>
+              <a href="/">Pricing</a>
+            </li>
+            <li>
+              <a href="/">Install</a>
+            </li>
+            <li>
+              <a href="/">Account</a>
+            </li>
+          </ul>
+        </nav>
       </header>
-    </div>
+
+      <main>
+        <section>
+          <hgroup>
+            <h1>Every little thing it does is magic</h1>
+            <p>A magical design tool for Figma powered by AI.</p>
+          </hgroup>
+          <a href="/">Install on Figma</a>
+          <img src="/" alt="/" />
+          <a href="/">2.1k likes</a>
+          <a href="/">53.3k installs</a>
+          <p>Made by Works with</p>
+        </section>
+
+        <section>
+          <hgroup>
+            <h1>Wave Your Magic Wand</h1>
+            <p>
+              Design with the power of AI to do everything from copywriting to
+              generating unique icons from text.
+            </p>
+          </hgroup>
+          <ul>
+            <li>Text to Icon</li>
+            <li>Copywriting</li>
+            <li>Text to Image</li>
+            <li>+ more</li>
+          </ul>
+          <picture>
+            <img src="/" alt="/" />
+            <img src="/" alt="/" />
+            <img src="/" alt="/" />
+          </picture>
+        </section>
+
+        <section>
+          <hgroup>
+            <h1>Cast Magic Spells</h1>
+            <p>
+              Each magic spell works alongside you to expand your creativity and
+              imagination as you design.
+            </p>
+          </hgroup>
+          <ul>
+            <li>Magic Icon</li>
+            <li>Magic Copy</li>
+            <li>Magic Image</li>
+            <li>+ more</li>
+          </ul>
+          <picture>
+            <img src="/" alt="/" />
+            <img src="/" alt="/" />
+            <img src="/" alt="/" />
+          </picture>
+        </section>
+
+        <section>
+          <hgroup>
+            <h1>Made by Diagram</h1>
+            <p>Be more creative with smarter design tools.</p>
+          </hgroup>
+          <picture>
+            <img src="/" alt="/" />
+            <img src="/" alt="/" />
+          </picture>
+          <hgroup>
+            <span>Used by designers on these teams:</span>
+            <img src="/" alt="/" />
+            <img src="/" alt="/" />
+            <img src="/" alt="/" />
+            <img src="/" alt="/" />
+            <img src="/" alt="/" />
+            <img src="/" alt="/" />
+            <img src="/" alt="/" />
+            <img src="/" alt="/" />
+            <img src="/" alt="/" />
+          </hgroup>
+        </section>
+
+        <section>
+          <h1>Pricing</h1>
+          <div>
+            <span>Public beta</span>
+            <hgroup>
+              <h3>Free</h3>
+              <p>1 license, unlimited use.</p>
+            </hgroup>
+            <ul>
+              <li>Early access</li>
+              <li>All magic spells</li>
+              <li>All future updates</li>
+            </ul>
+            <a href="/">Try for free</a>
+          </div>
+        </section>
+
+        <section>
+          <hgroup>
+            <h1>Get Started with Magician</h1>
+            <p>A magical design tool for Figma powered by AI.</p>
+          </hgroup>
+          <a href="/">Install on Figma</a>
+          <a href="/">Join the Discord</a>
+          <hgroup>
+            <h3>Magician</h3>
+            <ul>
+              <li>Support</li>
+              <li>Contact</li>
+              <li>Privacy</li>
+              <li>Terms</li>
+            </ul>
+          </hgroup>
+        </section>
+      </main>
+    </body>
   );
 }
